e31de35498d312fb4dea913f33983ecea741eb1e
Martin Grunbaum <martin@itsolveonline.net>
2011-07-29 04:06:04 +0200

    Added some new OrderType's

--- src/com/cphse/queue/OrderType.java
+++ src/com/cphse/queue/OrderType.java
@@ -1,5 +1,7 @@
 package com.cphse.queue;
 
 public enum OrderType {
+	MAIL_ACCOUNT_FETCH, // Fetch from a mail account 
+	FETCH_MAIL_ACCOUNTS, // Trigger order creation for all valid mail accounts not already triggered.
 	TEST
 }
